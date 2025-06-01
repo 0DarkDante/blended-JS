4d5d437a2e3cd00a196321a984ca760ebeb56f94
Vadik <vadym.kulinich7@gmail.com>
2025-06-01 21:24:40 +0300

    Ex 2

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -7,7 +7,7 @@
 // Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
 // перетворіть значення на число за допомогою Number().
 
-function checkNumber(params) {
+function checkNumber() {
   let number = Number(prompt('Введіть число'));
 
   if (number === 10) {
@@ -19,3 +19,29 @@ function checkNumber(params) {
 }
 
 checkNumber();
+
+// У змінній min лежить випадкове число від 0 до 59.
+// Визначте, в яку чверть години потрапляє
+// це число (у першу, другу, третю чи четверту).
+// Виведіть в alert відповідне повідомлення, наприклад:
+// "10 входить в першу чверть"
+
+// const min = Math.floor(Math.random() * (59 - 0) + 0);
+
+const min = Math.floor(Math.random() * (59 - 0) + 0);
+
+function checkNumber2() {
+  if (min <= 15) {
+    alert(`${min} входить в першу чверть`);
+  } else if (min > 15 && min <= 30) {
+    alert(`${min} входить в другу чверть`);
+  } else if (min > 30 && min <= 45) {
+    alert(`${min} входить в третю чверть`);
+  } else if (min > 45 && min < 60) {
+    alert(`${min} входить в четврету чверть`)
+  } else {
+    alert('Число не валідне');
+  };
+}
+
+// checkNumber2()
