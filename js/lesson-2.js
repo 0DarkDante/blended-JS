8c86bb272dec760b5cd4d1f653c724121274d52b
Vadik <vadym.kulinich7@gmail.com>
2025-06-26 11:43:16 +0300

    lesson 2 Ex4 finish

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -78,16 +78,27 @@ function caclculateAverage(...rest) {
     }
   };
 
-  return sum;
+  return sum/rest.length;
 }
 
-console.log( caclculateAverage(5, 'afs', 12));
+// console.log(caclculateAverage(5, 3, 12));
 
-// Напишіть функцію, яка сумуватиме сусідні числа 
+// Напишіть функцію, яка сумуватиме сусідні числа
 // і пушитиме їх в новий масив.
 
-// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
 
-// уточнення: складати необхідно перше число з другим, потім друге - з третім, 
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
 // третє - з четвертим і так до кінця.
 // В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+function sumAdjacentPairs(array) {
+  let arr1 = [];
+
+  for (let i = 0; i < array.length-1; i++) {
+    arr1.push(array[i] + array[i + 1]);
+  }
+  return arr1;
+}
+
+console.log(sumAdjacentPairs(someArr));
