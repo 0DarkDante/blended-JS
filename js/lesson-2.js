831e2252d32275da9a4ea17b9fbb5e4326d1c07a
Vadik <vadym.kulinich7@gmail.com>
2025-06-27 22:58:13 +0300

    lesson 2 Ex7

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -145,4 +145,18 @@ function findLongestWord(string) {
   return bigStr;  
 }
 
-console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
