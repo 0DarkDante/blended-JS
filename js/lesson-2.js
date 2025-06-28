7b78457812472c98948cd7b88bad819a8e81abbd
Vadik <vadym.kulinich7@gmail.com>
2025-06-28 13:06:54 +0300

    lesson 2 Ex10

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -227,10 +227,28 @@ let calculator = {
   }
 }
 
-calculator.read(2, 6);
+// calculator.read(2, 6);
 
-console.log(calculator.sum());
-console.log(calculator.mult());
+// console.log(calculator.sum());
+// console.log(calculator.mult());
+
+// Напишіть функцію calcTotalPrice(fruits, fruitName),
+// яка приймає массив об'єктів (fruits) і рядок з назвою фрукта (fruitName).
+// Функція рахує і повертає загальну вартість фрукта
+// з таким ім'ям, ціною та кількістю з об'єкта.
+
+// Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою 
+// назвою фрукта, це також треба урахувати.
+
+// const fruits = [
+//     { name: "Яблуко", price: 45, quantity: 7 },
+//     { name: "Апельсин", price: 60, quantity: 4 },
+//     { name: "Банан", price: 125, quantity: 8 },
+//     { name: "Груша", price: 350, quantity: 2 },
+//     { name: "Виноград", price: 440, quantity: 3 },
+//     { name: "Банан", price: 125, quantity: 3 },
+//   ];
+
 
 
 
